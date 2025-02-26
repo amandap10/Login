e44c425c42e0b1f582f2744a49bf004dc0c24cba
Amanda Oliveira <amanda_passos@outlook.com>
2025-02-26 07:27:52 -0300

    updates

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -15,39 +15,49 @@ const Login = () => {
     }
 
   return (
+    <>
     <div className='container'>
       <form onSubmit={handleSubmit}>
         <h2>Acesse o Sistema</h2>
-        <div className="container-one">
-            <FaUser className="icon"/>
-            {/* Função "onChange" vai captar os dados preenchidos dos usuários a partir do momento que for inserido e enviado */}
-            <input className="login" type="text" placeholder='Username'
-            onChange={(e) => setUsername(e.target.value)}
-            />
-        </div>
-        <div className="container-one">
-            <FaLock className="icon"/>
-            <input className="login" type="password" placeholder='Senha' 
-            onChange={(e) => setPassword(e.target.value)}
-            />
+        <div className="container-form">
+          <div className="container-one">
+              <FaUser className="icon"/>
+              {/* Função "onChange" vai captar os dados preenchidos dos usuários a partir do momento que for inserido e enviado */}
+              <input className="login" type="text" placeholder='Username'
+              onChange={(e) => setUsername(e.target.value)}
+              />
+          </div>
+          <div className="container-one">
+              <FaLock className="icon"/>
+              <input className="login" type="password" placeholder='Senha' 
+              onChange={(e) => setPassword(e.target.value)}
+              />
+          </div>
         </div>
         <div className="enter">
           <button>LOGIN</button>
         </div>
         <div className="recall-forget">
-            <label>
-                <input type="checkbox"/>
+            <label >
+                <input className="check" type="checkbox"/>
                 Lembre de mim?
             </label>
             <a href="#">Esqueceu a senha?</a>
         </div>
         
+        <div className="line"></div>
 
         <div className="signup-link">
             <p>Não tem uma conta? <a href="#">Registrar</a></p>
+            <p>ou</p>
+            <p></p>
         </div>
       </form>
+      
     </div>
+
+    <footer>&copy; 2025 <a href="https://comandolog.com.br/">Comando</a>. Todos os direitos reservados.</footer>
+    </>
   )
 }
 
